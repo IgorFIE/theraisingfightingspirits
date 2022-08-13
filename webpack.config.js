bce76c715765426c9f7c4aba0ac0dfa7ce802968
IgorFIE <igorfie@hotmail.com>
2022-08-13 20:05:42 +0100

    project starting point

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/main.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: './dist',
+  },
+};
